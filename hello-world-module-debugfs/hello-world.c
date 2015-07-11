21ac06d62d4df19df7f4e36f6b1f73cf16412e7b
Mauricio Garcia <mauriciojgv@gmail.com>
2015-07-11 12:43:46 -0500

    Add foo debugfs file

--- hello-world-module-debugfs/hello-world.c
+++ hello-world-module-debugfs/hello-world.c
@@ -5,6 +5,7 @@
 #include <linux/slab.h>
 #include <linux/debugfs.h>
 #include <linux/jiffies.h>
+#include <linux/mutex.h>
 
 MODULE_LICENSE("GPL");
 
@@ -12,6 +13,11 @@ static const char *msg = "75d9c1b2b032";
 static const size_t len_msg = 12;
 
 static struct dentry *dir;
+
+static char data[PAGE_SIZE];
+static int data_len;
+
+DEFINE_MUTEX(foo_mutex);
 
 static ssize_t device_read(struct file *filp, char *buffer,
 	size_t length, loff_t *offset)
@@ -67,6 +73,47 @@ const struct file_operations jiffies_fops = {
 	.read = read_jiffies
 };
 
+static ssize_t foo_read(struct file *filp, char *buffer,
+	size_t length, loff_t *offset)
+{
+	if (mutex_lock_interruptible(&foo_mutex))
+		return -EINTR;
+
+	int res = simple_read_from_buffer(buffer, length, offset, data,
+		data_len);
+
+	mutex_unlock(&foo_mutex);
+
+	return res;
+}
+
+static ssize_t foo_write(struct file *filp, const char *buffer,
+	size_t length, loff_t *off)
+{
+	if (mutex_lock_interruptible(&foo_mutex))
+		return -EINTR;
+
+	data_len = length;
+	if (data_len > PAGE_SIZE)
+		data_len = PAGE_SIZE;
+
+	ssize_t count = simple_write_to_buffer(data, PAGE_SIZE, off,
+		buffer, length);
+
+	int res = length;
+
+	if (count != data_len)
+		res = -EFAULT;
+
+	mutex_unlock(&foo_mutex);
+	return res;
+}
+
+const struct file_operations foo_fops = {
+	.read = foo_read,
+	.write = foo_write,
+};
+
 static int __init my_init_module(void)
 {
 	pr_info("Hello World!");
@@ -86,6 +133,12 @@ static int __init my_init_module(void)
 	jiffies_file = debugfs_create_file("jiffies", 0444, dir, NULL,
 		&jiffies_fops);
 	if (!jiffies_file)
+		return -EPERM;
+
+	struct dentry *foo_file = NULL;
+
+	foo_file = debugfs_create_file("foo", 0644, dir, NULL,  &foo_fops);
+	if (!foo_file)
 		return -EPERM;
 
 	return 0;
